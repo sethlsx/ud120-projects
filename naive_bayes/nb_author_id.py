b7bec061adc0d5d2d3025ad5c3048aef930ae8aa
sethlsx <sethl@qq.com>
2018-01-10 16:22:21 +0800

    Add time lines

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,8 +29,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0,3), "s")
+t0 = time()
 pred = clf.predict(features_test)
+print("predictation time:", round(time()-t0,3), "s")
 accuracy_ = accuracy_score(labels_test, pred)
 print('The accuracy of the Naive Bayes method is {}'.format(accuracy_))
 
