8f4e6bd9cf1c1d5fd5ee650bc3d1dac7a390c0e2
sethlsx <sethl@qq.com>
2018-01-11 21:06:02 +0800

    Restore the data set to original size instead 1% of the original

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -30,8 +30,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 clf = SVC(C = 10000.0, kernel="rbf")
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0,3), "s"
