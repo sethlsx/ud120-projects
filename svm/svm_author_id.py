7d116d06b63618c9d532ab2ea3cebefa856f15b9
sethlsx <sethl@qq.com>
2018-01-11 20:56:21 +0800

    Change kernel from linear to rbf.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -29,7 +29,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf")
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
 t0 = time()
