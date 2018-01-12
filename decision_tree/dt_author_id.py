2126a66469abb759ca3257c840a63b0768174664
sethlsx <sethl@qq.com>
2018-01-12 15:00:30 +0800

    Fill in the code for decision tree method and caculate the accuracy and traing/fit time

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+time0 = time()
+clf.fit(features_train, labels_train)
+print 'The training time is: {}'.format(round(time()-time0,3)), 's'
+time0 = time()
+pred = clf.predict(features_test)
+print 'The prediction time is: {}'.format(round(time()-time0,3)), 's'
+acc = accuracy_score(labels_test, pred)
+print "The accuracy of decision tree method is: {}".format(acc)
 #########################################################
 
 
